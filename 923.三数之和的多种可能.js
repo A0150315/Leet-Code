01cb2b260fcad9cac68f9d31edead3740994d58e
谭健青 <574469551@qq.com>
2019-05-29 11:14:46 +0800

    python

--- 923.三数之和的多种可能.js
+++ 923.三数之和的多种可能.js
@@ -8,38 +8,73 @@
  * @param {number} target
  * @return {number}
  */
+
+// 三种情况：
+// a==b==c C(n,3)
+// a==b!=c C(n,2)*C(m)
+// a<b and b<c C(a)*C(b)*C(c)
+
 var threeSumMulti = function(A, target) {
-  const M = Math.pow(10, 9) + 7
-  // 复杂度O(n^2)
-  A = A.sort((a, b) => a - b)
-  let res = 0
-  for (let k = 0; k + 2 < A.length; k++) {
-    // 第一个数为A[k]
-    let i = k + 1,
-      j = A.length - 1
-    while (i < j) {
-      if (A[i] + A[j] > target - A[k]) {
-        j--
-      } else if (A[i] + A[j] == target - A[k]) {
-        if (A[i] == A[j]) {
-          // C(j-i+1,2)
-          res = (res + ((j - i + 1) * (j - i)) / 2) % M
-          break
-        } else {
-          let ti = i,
-            tj = j
-          while (ti + 1 < tj && A[ti] == A[ti + 1]) ti++
-          while (ti < tj - 1 && A[tj] == A[tj - 1]) tj--
-          res = (res + (ti - i + 1) * (j - tj + 1)) % M
-          i = ti
-          i++
-          j = tj
-          tj--
-        }
-      } else {
-        i++
-      }
-    }
+  let store = []
+  let i
+  let result = 0
+  for (i = 0; i <= target; i++) {
+    store[i] = 0
   }
-  return res
+  for (i = A.length - 1; i >= 0; i--) {
+    if (A[i] <= target) store[A[i]]++
+  }
+
+  //store 下标为数字，值为数字对应的数量
+
+  for (i = 0; i < target / 3; i++) {
+    for (let j = i + 1; j <= target / 2; j++) {
+      if (target - i - j > j) {
+        result = result + store[i] * store[j] * store[target - i - j]
+        continue
+      }
+      if (target - i - j == j)
+        result = result + ((store[j] * (store[j] - 1)) / 2) * store[i]
+    }
+    result = result + ((store[i] * (store[i] - 1)) / 2) * store[target - 2 * i]
+  }
+  if (target % 3 === 0) {
+    return (
+      (result +
+        (store[target / 3] *
+          (store[target / 3] - 1) *
+          (store[target / 3] - 2)) /
+          6) %
+      (Math.pow(10, 9) + 7)
+    )
+  }
+  return result % (Math.pow(10, 9) + 7)
 }
+
+threeSumMulti([1, 1, 2, 2, 2, 2], 5)
+
+
+// python 
+// from collections import Counter
+// class Solution:
+//     def threeSumMulti(self, A, target):
+//         """
+//         :type A: List[int]
+//         :type target: int
+//         :rtype: int
+//         """
+//         c = Counter(A)
+//         result = 0
+//         for i, x in c.items():
+//             for j, y in c.items():
+//                 k = target - i - j
+//                 if k not in c:
+//                     continue
+//                 if i == j == k: 
+//                     result += x * (x - 1) * (x - 2) // 6
+//                 elif i == j != k: 
+//                     result += x * (x - 1) // 2 * c[k]
+//                 elif i < j and j < k: 
+//                     result += x * y * c[k]
+
+//         return result % (10**9 + 7)
